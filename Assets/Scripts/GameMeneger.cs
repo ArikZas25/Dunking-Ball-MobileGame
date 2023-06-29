5b8ef5d603bfaac7b7f85b911c38345bf60f6b12
ArikZas25 <Arik2510@gmail.com>
2023-06-29 18:21:29 +0300

    Menu scene

--- Assets/Scripts/GameMeneger.cs
+++ Assets/Scripts/GameMeneger.cs
@@ -2,17 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UIElements;
 
 public class GameMeneger : MonoBehaviour
 {
     [SerializeField] private TMP_Text GameOverText;
+    [SerializeField] private TextMeshProUGUI MenuButton;
     [SerializeField] private GameObject player;
 
+
+    private void Start()
+    {
+        MenuButton.enabled = false;
+    }
     void Update()
     {
         if (destroy.RingIsOut)
         {
             GameOverText.enabled = true;
+            MenuButton.enabled = true;
             player.GetComponent<movmentforplayer>().enabled = false;
         }
     }
