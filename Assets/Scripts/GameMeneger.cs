f86d94ae8bbc16011f2d72556cd65a244b011357
ArikZas25 <Arik2510@gmail.com>
2023-06-30 22:28:50 +0300

    new  sizes
    trying new sizes for the menu floor and player

--- Assets/Scripts/GameMeneger.cs
+++ Assets/Scripts/GameMeneger.cs
@@ -3,25 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UIElements;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
 
 public class GameMeneger : MonoBehaviour
 {
     [SerializeField] private TMP_Text GameOverText;
-    [SerializeField] private TextMeshProUGUI MenuButton; //does not conect to the tmpro button
+    [SerializeField] public GameObject MenuButton; //does not conect to the tmpro button
     [SerializeField] private GameObject player;
 
 
     private void Start()
     {
-        MenuButton.enabled = false;
+        MenuButton.SetActive(false);
     }
     void Update()
     {
         if (destroy.RingIsOut)
         {
             GameOverText.enabled = true;
-            MenuButton.enabled = true;
+            MenuButton.SetActive(true);
             player.GetComponent<movmentforplayer>().enabled = false;
         }
+    }
+
+    public void ChangeScene(string LoadScene)
+    {
+        SceneManager.LoadScene(0);
     }
 }
