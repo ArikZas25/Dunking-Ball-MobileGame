405d55a049030b2bd4a7d7f0c9a144380749354c
ArikZas25 <Arik2510@gmail.com>
2023-07-04 15:05:20 +0300

    cleaning the code
    - not needed

--- Assets/Scripts/menuMenegerScript.cs
+++ Assets/Scripts/menuMenegerScript.cs
@@ -6,7 +6,7 @@ public class menuMenegerScript : MonoBehaviour
 {
     public static bool clickOnButton;
 
-    audioManagerScript audioManager;
+ 
     public void ChangeScene(string LoadScene)
     {
         SceneManager.LoadScene("game scene");
@@ -14,21 +14,8 @@ public class menuMenegerScript : MonoBehaviour
                               // GetComponent<movmentforplayer>().enabled = true;
         NewGame1();
     }
-
-    private void Awake()
-    {
-        audioManager = GameObject.FindGameObjectWithTag("Audio").GetComponent<audioManagerScript>();
-    }
     public void NewGame1()
     {
         destroy.RingIsOut = false;
-    }
-
-    private void Update()
-    {
-        if (Time.time == 0f)
-        {
-            audioManager.playSFX(audioManager.backgroundmusic);
-        }
     }
 }
